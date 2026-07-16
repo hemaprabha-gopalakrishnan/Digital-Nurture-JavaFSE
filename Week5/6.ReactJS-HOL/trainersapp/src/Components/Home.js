b5a163d929c46adb1ce82847586a8eb29b312257
HEMA PRABHA G <155607228+hemaprabha-gopalakrishnan@users.noreply.github.com>
2026-07-16 11:28:07 +0530

    React 6,7 exercise uploaded

---
+++ Week5/6.ReactJS-HOL/trainersapp/src/Components/Home.js
@@ -0,0 +1,17 @@
+function Home(){
+
+    return(
+
+        <div>
+
+            <h2>Welcome to Trainers Application</h2>
+
+            <p>Select Trainers from menu.</p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
---
+++ Week5/6.ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,34 @@
+import Trainer from "./trainer";
+
+const trainers=[
+
+new Trainer(
+1,
+"John",
+"john@gmail.com",
+"9876543210",
+"Java",
+["Core Java","Spring","Hibernate"]
+),
+
+new Trainer(
+2,
+"David",
+"david@gmail.com",
+"9876543211",
+".NET",
+["C#",".NET Core","MVC"]
+),
+
+new Trainer(
+3,
+"Peter",
+"peter@gmail.com",
+"9876543212",
+"React",
+["React","JavaScript","HTML","CSS"]
+)
+
+];
+
+export default trainers;
